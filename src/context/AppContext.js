33a69e7e435dab4dd84aa7fece2ddb4a9beafd03
Jamal <synsensi@gmail.com>
2023-02-18 08:30:49 -0500

    Updated and added components to app container.

--- src/context/AppContext.js
+++ src/context/AppContext.js
@@ -1,10 +1,10 @@
-import React, { createContext, useReducer } from 'react';
+import React, { createContext, useReducer } from "react";
 
 // 5. The reducer - this is used to update the state, based on the action
 export const AppReducer = (state, action) => {
     let budget = 0;
     switch (action.type) {
-        case 'ADD_EXPENSE':
+        case "ADD_EXPENSE":
             let total_budget = 0;
             total_budget = state.expenses.reduce(
                 (previousExp, currentExp) => {
@@ -30,7 +30,7 @@ export const AppReducer = (state, action) => {
                     ...state
                 }
             }
-            case 'RED_EXPENSE':
+            case "RED_EXPENSE":
                 const red_expenses = state.expenses.map((currentExp)=> {
                     if (currentExp.name === action.payload.name && currentExp.cost - action.payload.cost >= 0) {
                         currentExp.cost =  currentExp.cost - action.payload.cost;
@@ -43,7 +43,7 @@ export const AppReducer = (state, action) => {
                     ...state,
                     expenses: [...red_expenses],
                 };
-            case 'DELETE_EXPENSE':
+            case "DELETE_EXPENSE":
             action.type = "DONE";
             state.expenses.map((currentExp)=> {
                 if (currentExp.name === action.payload) {
@@ -57,14 +57,14 @@ export const AppReducer = (state, action) => {
                 ...state,
                 budget
             };
-        case 'SET_BUDGET':
+        case "SET_BUDGET":
             action.type = "DONE";
             state.budget = action.payload;
 
             return {
                 ...state,
             };
-        case 'CHG_CURRENCY':
+        case "CHG_CURRENCY":
             action.type = "DONE";
             state.currency = action.payload;
             return {
@@ -80,13 +80,13 @@ export const AppReducer = (state, action) => {
 const initialState = {
     budget: 2000,
     expenses: [
-        { id: "Marketing", name: 'Marketing', cost: 50 },
-        { id: "Finance", name: 'Finance', cost: 300 },
-        { id: "Sales", name: 'Sales', cost: 70 },
-        { id: "Human Resource", name: 'Human Resource', cost: 40 },
-        { id: "IT", name: 'IT', cost: 500 },
+        { id: "Marketing", name: "Marketing", cost: 50 },
+        { id: "Finance", name: "Finance", cost: 300 },
+        { id: "Sales", name: "Sales", cost: 70 },
+        { id: "Human Resource", name: "Human Resource", cost: 40 },
+        { id: "IT", name: "IT", cost: 500 },
     ],
-    currency: '£'
+    currency: "£"
 };
 
 // 2. Creates the context this is the thing our components import and use to get the state
